465c60866c79ef13c2e90c6be8bc56c583febb80
Elena Oleksun <candyflos2011@gmail.com>
2022-02-08 17:35:41 +0200

    add login form

--- src/app/pages/login-page/shared/login-form/login-form.module.ts
+++ src/app/pages/login-page/shared/login-form/login-form.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import {SharedModule} from "@shared/modules";
 import { LoginFormComponent } from './login-form.component';
+import {LayoutModule} from "@layout/layout.module";
 
 @NgModule({
   declarations: [LoginFormComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, SharedModule, LayoutModule],
   exports: [LoginFormComponent],
 })
 export class LoginFormModule {}
